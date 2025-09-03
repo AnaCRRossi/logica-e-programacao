d40eb2b59d332128532375050b9682f22ec630da
Ana Carolina <anacarolinacroncadelima@gmail.com>
2025-09-03 19:12:48 -0300

    Soma de dois números zerados e de dois números negativos

--- test/calculadora.tests.js
+++ test/calculadora.tests.js
@@ -17,4 +17,20 @@ describe('Teste da Função de Soma', function() {
         // Compara o resultado com o valor que você espera
         expect(resultadoDaSoma).to.equal(35);
     });
+
+    it('A função deve ser capaz de somar dois números zerados', function() {
+        // Coleta o resultado da função
+        const resultadoDaSoma = somarDoisNumeros(0,0);
+
+        // Compara o resultado com o valor que você espera
+        expect(resultadoDaSoma).to.equal(0);
+    });    
+
+    it('A função deve ser capaz de somar dois números negativos', function() {
+        // Coleta o resultado da função
+        const resultadoDaSoma = somarDoisNumeros(-20,-15);
+
+        // Compara o resultado com o valor que você espera
+        expect(resultadoDaSoma).to.equal(-35);
+    });    
 }); 
